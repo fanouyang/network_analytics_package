ba034b39da54f7365ff1b8c9b0d2be310e851efb
Fan Ouyang <ouyan064@umn.edu>
2017-10-04 08:19:13 -0500

    update files

---
+++ R/Data.R
@@ -0,0 +1,83 @@
+#' Week 02's participant to participant network format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 02 discussion including 20 graduate students and 1 instructor
+#' @format A network with multiple attributes, such as vertices, directed, bipartite, total edges, etc.
+#' @examples
+#' \dontrun{
+#'  week02_network
+#' }
+"week02_network"
+
+
+#' Week 02's participant to participant matrix format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 02 discussion including 20 graduate students and 1 instructor
+#' @format A square-format participant-by-participant matrix
+#' @examples
+#' \dontrun{
+#'  week02_matrix
+#' }
+"week02_matrix"
+
+#' Week 08's participant to participant network format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 08 discussion including 20 graduate students and 1 instructor
+#' @format A network with multiple attributes, such as vertices, directed, bipartite, total edges, etc.
+#' @examples
+#' \dontrun{
+#'  week08_network
+#' }
+"week08_network"
+
+
+#' Week 08's participant to participant matrix format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 08 discussion including 20 graduate students and 1 instructor
+#' @format A square-format participant-by-participant matrix
+#' @examples
+#' \dontrun{
+#'  week08_matrix
+#' }
+"week08_matrix"
+
+#' Week 13's participant to participant network format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 13 discussion including 20 graduate students and 1 instructor
+#' @format A network with multiple attributes, such as vertices, directed, bipartite, total edges, etc.
+#' @examples
+#' \dontrun{
+#'  week13_network
+#' }
+"week13_network"
+
+
+#' Week 13's participant to participant matrix format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 02 discussion including 20 graduate students and 1 instructor
+#' @format A square-format participant-by-participant matrix
+#' @examples
+#' \dontrun{
+#'  week13_matrix
+#' }
+"week13_matrix"
+
+#' Week 14's participant to participant network format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 14 discussion including 20 graduate students and 1 instructor
+#' @format A network with multiple attributes, such as vertices, directed, bipartite, total edges, etc.
+#' @examples
+#' \dontrun{
+#'  week14_network
+#' }
+"week14_network"
+
+
+#' Week 14's participant to participant matrix format file
+#'
+#' @source Fan Ouyang dissertation research, participants in week 14 discussion including 20 graduate students and 1 instructor
+#' @format A square-format participant-by-participant matrix
+#' @examples
+#' \dontrun{
+#'  week14_matrix
+#' }
+"week14_matrix"
---
+++ R/Functions.R
@@ -0,0 +1,76 @@
+#' Plot sociographs for networks.
+#'
+#' \code{sna_plot} plot sociograph for a network.
+#'
+#' This function plots sociograph for a network in fruchtermanreingold mode, with node size and color representing
+#' a participant's degree (indegree and outdegree) and edge width representing the frequency of interactions between
+#' two participants
+#'
+#' @param x a network
+#' @param y a matrix
+#' @example
+#' sna_plot(week02_network,week02_matrix)
+#'
+#' @export
+sna_plot<-function(x,y){
+  require(sna)
+  od<-degree(x,gmode="digraph",cmode="outdegree")
+  id<-degree(x,gmode="digraph",cmode="indegree")
+  d<-od+id
+  gplot(x,vertex.cex=d^0.5, gmode="graph",boxed.labels=FALSE,
+        label.cex=0.7, label.pos=5, label.col="grey17",
+        vertex.col=rgb((d)/max(d),0,(d)/max(d)),edge.col="grey17",
+        label=network.vertex.names(x),
+        edge.lwd=y*3,mode = "fruchtermanreingold")
+}
+
+#' Return node-level measures of a network.
+#'
+#' \code{node_measure} calculate node-level measures of a network.
+#'
+#' This function returns a network's node-level measures, namely every participant's in/outdegree, betweenness,
+#' closeness.
+#'
+#' @param x a network
+#' @example
+#' node_measure(week02_network)
+#'
+#' @export
+node_measure<-function(x){
+  require(sna)
+  central.nodes<-cbind(degree(x,cmode="indegree"), degree(x,cmode="outdegree"), betweenness(x,rescale=T),
+                       closeness(x,cmode="directed",rescale=T))
+  colnames(central.nodes)<-c("indegree","outdegree","betweenness","closeness")
+  rownames(central.nodes)<-x%v%"vertex.names"
+
+  list(ranking=central.nodes)
+}
+
+#' Return network-level measures of a network.
+#'
+#' \code{network_measure} calculate network-level measures of a network.
+#'
+#' This function returns results of a network's network-level measures, including network size, density,
+#' average degree, number of edges, reciprocity, transitivity, centralization, hierarchy,
+#' number of components, connectedness
+#'
+#' @param x a network
+#' @example
+#' node_measure(week02_network)
+#'
+#' @export
+
+network_measure<-function(x){
+  require(sna)
+  network.measure<-cbind(network.size(x),gden(x,mode="graph"),mean(degree(x)),
+                         network.edgecount(x,na.omit = F),
+                         grecip(x, measure = "dyadic.nonnull"),gtrans(x),
+                         centralization(x,degree),hierarchy(x, measure = "reciprocity"),
+                         components(x,connected="weak"),connectedness(x, g=NULL))
+
+  colnames(network.measure)<-c("network size","density","average degree",
+                               "number of edges","reciprocity","transitivity",
+                               "centralization","hierarchy",
+                               "number of components","connectedness")
+  list(network.measure)
+}
