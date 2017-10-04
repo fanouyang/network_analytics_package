51ebd3529352376772b9628401e841f6984703d3
Fan Ouyang <ouyan064@umn.edu>
2017-10-04 08:36:59 -0500

    run tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,18 @@ library(testthat)
 library(networkanalyticspackage)
 
 test_check("networkanalyticspackage")
+
+test_that("sna plot", {
+   p1 <- sna_plot(week02_network, week02_matrix)
+   expect_that(p1, is_a("matrix"))
+})
+
+test_that("node measure", {
+  node1 <- node_measure(week02_network)
+  expect_that(node1, is_a("list"))
+})
+
+test_that("network measure", {
+  net1 <- network_measure(week02_network)
+  expect_that(net1, is_a("list"))
+})
